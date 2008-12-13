2f78223e632dbf55cf549fc4b7185e3b811e469e
Nedjo Rogers <code@chocolatelilyweb.ca>
2008-12-13 02:01:29 +0000

    Issue #277464 with contributions from Tanc and Vote_Sizing_Steve and #345684. Change to using a single cookie with JSON-encoded data. Fix bug with cookie behaviour. Add new always collapsed option. Add configurable animation.

--- collapsiblock.js
+++ collapsiblock.js
@@ -1,39 +1,68 @@
 // $Id$
 
 Drupal.behaviors.collapsiblock = function (context) {
+  var cookieSt = Drupal.parseJson($.cookie('collapsiblock')) || {};
+  var slidetype = Drupal.settings.collapsiblock.slide_type;
+  var defaultState = Drupal.settings.collapsiblock.default_state;
+  var slidespeed = parseInt(Drupal.settings.collapsiblock.slide_speed);
   $('div.block:not(.collapsiblock-processed)', context).addClass('collapsiblock-processed').each(function () {
     var selector = Drupal.settings.collapsiblock && Drupal.settings.collapsiblock.titleSelector ? Drupal.settings.collapsiblock.titleSelector : 'h2';
     var id = this.id;
     var titleElt = $(selector +':first', this);
     if (titleElt.size()) {
       titleElt = titleElt[0];
-      // Status values: 1 = not collapsible, 2 = collapsible and expanded, 3 = collapsible and collapsed
-      var status = Drupal.settings.collapsiblock.blocks[this.id] ? Drupal.settings.collapsiblock.blocks[this.id] : Drupal.settings.collapsiblock.default_state;
-      if (status == 1) {
+      // Status values: 1 = not collapsible, 2 = collapsible and expanded, 3 = collapsible and collapsed, 4 = always collapsed
+      var stat = Drupal.settings.collapsiblock.blocks[this.id] ? Drupal.settings.collapsiblock.blocks[this.id] : defaultState;
+      if (stat == 1) {
         return;
       }
+
       titleElt.target = $(this).find('div.content');
       $(titleElt)
         .addClass('collapsiblock')
         .click(function () {
-          var st = $.cookie('collapsiblock-' + id);
-          $.cookie('collapsiblock-' + id, st == 0 ? 1 : 0, {path: Drupal.settings.jstools.basePath});
+          var st = Drupal.parseJson($.cookie('collapsiblock')) || {};
           if ($(this).is('.collapsiblockCollapsed')) {
             $(this).removeClass('collapsiblockCollapsed');
-            $(this.target).slideDown(200);
+            if (slidetype == 1) {
+              $(this.target).slideDown(slidespeed);
+            }
+            else {
+              $(this.target).animate({height:'show', opacity:'show'}, slidespeed);
+            }
+
+            // Don't save cookie data if the block is always collapsed.
+            if (stat != 4) {
+              st[id] = 1;
+            }
           } 
           else {
             $(this).addClass('collapsiblockCollapsed');
-            $(this.target).slideUp(200);
+            if (slidetype == 1) {
+              $(this.target).slideUp(slidespeed);
+            }
+            else {
+              $(this.target).animate({height:'hide', opacity:'hide'}, slidespeed);
+            }
+
+            // Don't save cookie data if the block is always collapsed.
+            if (stat != 4) {
+              st[id] = 0;
+            }
           }
+          // Stringify the object in JSON format for saving in the cookie.
+          var cookieString = '{ ';
+          var cookieParts = [];
+          $.each(st, function (id, setting) {
+            cookieParts[cookieParts.length] = ' "' + id + '": ' + setting;
+          });
+          cookieString += cookieParts.join(', ') + ' }';
+          $.cookie('collapsiblock', cookieString, {path: Drupal.settings.basePath});
         });
-      // Leave active blocks uncollapsed.
-      if (((status == 3 && (! $.cookie('collapsiblock-' + id))) || $.cookie('collapsiblock-' + id) == 1) && !$(this).find('a.active').size()) {
+      // Leave active blocks uncollapsed. If the block is expanded, do nothing.
+      if (stat ==  4 || (cookieSt[id] == 0 || (stat == 3 && cookieSt[id] == 'undefined')) && !$(this).find('a.active').size()) {
         $(titleElt).addClass('collapsiblockCollapsed');
         $(titleElt.target).hide();
-      }
-      else {
-        $.cookie('collapsiblock-' + id, 0, {path: Drupal.settings.jstools.basePath});
       }
     }
   });
