774c631b0f7efc4ae73099bd24e8608a1b00f80b
gagarine <perdrisat@gmail.com>
2011-04-18 01:07:52 +0200

    #892340 working D7 version. Patch by jamsilver.

--- collapsiblock.js
+++ collapsiblock.js
@@ -1,90 +1,106 @@
 
-Drupal.Collapsiblock = Drupal.Collapsiblock || {};
+(function($) {
 
-Drupal.behaviors.collapsiblock = function (context) {
-  var cookieData = Drupal.Collapsiblock.getCookieData();
-  var slidetype = Drupal.settings.collapsiblock.slide_type;
-  var defaultState = Drupal.settings.collapsiblock.default_state;
-  var slidespeed = parseInt(Drupal.settings.collapsiblock.slide_speed);
-  var title = Drupal.settings.collapsiblock.block_title;
-  var block = Drupal.settings.collapsiblock.block;
-  var block_content = Drupal.settings.collapsiblock.block_content;
-  $(block + ':not(.collapsiblock-processed)', context).addClass('collapsiblock-processed').each(function () {
-    var id = this.id;
-    var titleElt = $(title, this).not($('.content :header',this));
-    if (titleElt.size()) {
-      titleElt = titleElt[0];
-      // Status values: 1 = not collapsible, 2 = collapsible and expanded, 3 = collapsible and collapsed, 4 = always collapsed
-      var stat = Drupal.settings.collapsiblock.blocks[this.id] ? Drupal.settings.collapsiblock.blocks[this.id] : defaultState;
-      if (stat == 1) {
-        return;
-      }
+  Drupal.Collapsiblock = Drupal.Collapsiblock || {};
 
-      titleElt.target = $(this).find(block_content);
-      $(titleElt)
-      .wrapInner('<a href="#" role="link" />')
-      .addClass('collapsiblock')
-      .click(function (e) {
-        var st = Drupal.Collapsiblock.getCookieData();
-        if ($(this).is('.collapsiblockCollapsed')) {
-          $(this).removeClass('collapsiblockCollapsed');
-          if (slidetype == 1) {
-            $(this.target).slideDown(slidespeed).attr('aria-hidden', false); ;
-          }
-          else {
-            $(this.target).animate({
-              height:'show',
-              opacity:'show'
-            }, slidespeed);
+  Drupal.behaviors.collapsiblock = {
+
+    attach: function (context) {
+      var cookieData = Drupal.Collapsiblock.getCookieData();
+      var slidetype = Drupal.settings.collapsiblock.slide_type;
+      var defaultState = Drupal.settings.collapsiblock.default_state;
+      var slidespeed = parseInt(Drupal.settings.collapsiblock.slide_speed);
+      var title = Drupal.settings.collapsiblock.block_title;
+      var block = Drupal.settings.collapsiblock.block;
+      var block_content = Drupal.settings.collapsiblock.block_content;
+      $(block + ':not(.collapsiblock-processed)', context).addClass('collapsiblock-processed').each(function () {
+        var id = this.id.replace(/_/g, '-');
+        var titleElt = $(title, this).not($('.content :header',this));
+        if (titleElt.size()) {
+          titleElt = titleElt[0];
+          // Status values: 1 = not collapsible, 2 = collapsible and expanded, 3 = collapsible and collapsed, 4 = always collapsed
+          var stat = Drupal.settings.collapsiblock.blocks[id] ? Drupal.settings.collapsiblock.blocks[id] : defaultState;
+          if (stat == 1) {
+            return;
           }
 
-          // Don't save cookie data if the block is always collapsed.
-          if (stat != 4) {
-            st[id] = 1;
-          }
-        }
-        else {
-          $(this).addClass('collapsiblockCollapsed');
-          if (slidetype == 1) {
-            $(this.target).slideUp(slidespeed).attr('aria-hidden', true);
-          }
-          else {
-            $(this.target).animate({
-              height:'hide',
-              opacity:'hide'
-            }, slidespeed);
-          }
+          titleElt.target = $(this).find(block_content);
+          $(titleElt)
+          .wrapInner('<a href="#" role="link" />')
+          .addClass('collapsiblock')
+          .click(function (e) {
+            var st = Drupal.Collapsiblock.getCookieData();
+            if ($(this).is('.collapsiblockCollapsed')) {
+              $(this).removeClass('collapsiblockCollapsed');
+              if (slidetype == 1) {
+                $(this.target).slideDown(slidespeed).attr('aria-hidden', false); ;
+              }
+              else {
+                $(this.target).animate({
+                  height:'show',
+                  opacity:'show'
+                }, slidespeed);
+              }
 
-          // Don't save cookie data if the block is always collapsed.
-          if (stat != 4) {
-            st[id] = 0;
-          }
-        }
-        // Stringify the object in JSON format for saving in the cookie.
-        var cookieString = '{ ';
-        var cookieParts = [];
-        $.each(st, function (id, setting) {
-          cookieParts[cookieParts.length] = ' "' + id + '": ' + setting;
-        });
-        cookieString += cookieParts.join(', ') + ' }';
-        $.cookie('collapsiblock', cookieString, {
-          path: Drupal.settings.basePath
+              // Don't save cookie data if the block is always collapsed.
+              if (stat != 4) {
+                st[id] = 1;
+              }
+            }
+            else {
+              $(this).addClass('collapsiblockCollapsed');
+              if (slidetype == 1) {
+                $(this.target).slideUp(slidespeed).attr('aria-hidden', true);
+              }
+              else {
+                $(this.target).animate({
+                  height:'hide',
+                  opacity:'hide'
+                }, slidespeed);
+              }
+
+              // Don't save cookie data if the block is always collapsed.
+              if (stat != 4) {
+                st[id] = 0;
+              }
+            }
+            // Stringify the object in JSON format for saving in the cookie.
+            var cookieString = '{ ';
+            var cookieParts = [];
+            $.each(st, function (id, setting) {
+              cookieParts[cookieParts.length] = ' "' + id + '": ' + setting;
+            });
+            cookieString += cookieParts.join(', ') + ' }';
+            $.cookie('collapsiblock', cookieString, {
+              path: Drupal.settings.basePath
+            });
+            e.preventDefault();
           });
-        e.preventDefault();
+          // Leave active blocks uncollapsed. If the block is expanded, do nothing.
+          if (stat ==  4 || (cookieData[id] == 0 || (stat == 3 && cookieData[id] == undefined)) && !$(this).find('a.active').size()) {
+            // Allow block content to assign class 'collapsiblock-force-open' to it's content to force
+            // itself to stay open. E.g. useful if block contains a form that was just ajaxly updated and should be visible
+            if (titleElt.target.hasClass('collapsiblock-force-open') || titleElt.target.find('.collapsiblock-force-open').size() > 0) {
+              return;
+            }
+            $(titleElt).addClass('collapsiblockCollapsed');
+            $(titleElt.target).hide();
+          }
+        }
       });
-      // Leave active blocks uncollapsed. If the block is expanded, do nothing.
-      if (stat ==  4 || (cookieData[id] == 0 || (stat == 3 && cookieData[id] == undefined)) && !$(this).find('a.active').size()) {
-        $(titleElt).addClass('collapsiblockCollapsed');
-        $(titleElt.target).hide();
-      }
     }
-  });
-};
 
-Drupal.Collapsiblock.getCookieData = function () {
-  var cookieString = $.cookie('collapsiblock');
-  return cookieString ? Drupal.parseJson(cookieString) : {};
-};
+  };
 
-  
+  Drupal.Collapsiblock.getCookieData = function () {
+    if ($.cookie) {
+      var cookieString = $.cookie('collapsiblock');
+      return cookieString ? $.parseJSON(cookieString) : {};
+    }
+    else {
+      return '';
+    }
+  };
 
+
+})(jQuery);
