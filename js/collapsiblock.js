df36c9ce94f5b19c7184c287db35cd6b45ed3365
sonvir249 <choudharysonvir@gmail.com>
2017-12-05 13:57:45 +0530

    javascript changes

--- js/collapsiblock.js
+++ js/collapsiblock.js
@@ -5,11 +5,14 @@
 
 (function ($, Drupal, drupalSettings) {
 
+  'use strict';
+
   Drupal.Collapsiblock = Drupal.Collapsiblock || {};
 
   Drupal.behaviors.collapsiblock = {
 
     attach: function (context, settings) {
+      var undefined = 'undefined';
       var cookieData = Drupal.Collapsiblock.getCookieData();
       var slidetype = settings.collapsiblock.slide_type;
       var defaultState = settings.collapsiblock.default_state;
@@ -26,7 +29,7 @@
           // Status values: 1 = not collapsible, 2 = collapsible and expanded,
           // 3 = collapsible and collapsed, 4 = always collapsed.
           var stat = settings.collapsiblock.blocks[id] ? settings.collapsiblock.blocks[id] : defaultState;
-          if (stat == 1) {
+          if (stat === 1) {
             return;
           }
           titleElt.target = $(this).find(block_content);
@@ -38,7 +41,7 @@
             var st = Drupal.Collapsiblock.getCookieData();
             if ($(this).is('.collapsiblockCollapsed')) {
               $(this).removeClass('collapsiblockCollapsed');
-              if (slidetype == 1) {
+              if (slidetype === 1) {
                 $(this.target).slideDown(slidespeed).attr('aria-hidden', false);
               }
               else {
@@ -49,13 +52,13 @@
               }
 
               // Don't save cookie data if the block is always collapsed.
-              if (stat != 4) {
+              if (stat !== 4) {
                 st[id] = 1;
               }
             }
             else {
               $(this).addClass('collapsiblockCollapsed');
-              if (slidetype == 1) {
+              if (slidetype === 1) {
                 $(this.target).slideUp(slidespeed).attr('aria-hidden', true);
               }
               else {
@@ -66,7 +69,7 @@
               }
 
               // Don't save cookie data if the block is always collapsed.
-              if (stat != 4) {
+              if (stat !== 4) {
                 st[id] = 0;
               }
             }
@@ -86,7 +89,7 @@
           });
           // Leave active blocks if Remember collapsed on active pages is false.
           // If the block is expanded, do nothing.
-          if (stat == 4 || (cookieData[id] == 0 || (stat == 3 && cookieData[id] == undefined))) {
+          if (stat === 4 || (cookieData[id] === 0 || (stat === 3 && cookieData[id] === undefined))) {
             if (!$(this).find('a.active').size() || activePages === 1) {
               // Allow block content to assign class 'collapsiblock-force-open'
               // to it's content to force itself to stay open. E.g. useful if
